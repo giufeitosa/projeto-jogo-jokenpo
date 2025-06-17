c11a03d13ab795a4f2afdf97a7cca60808724687
Giu <giuslaine.feitosa01@gmail.com>
2025-06-17 18:32:09 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playHuman = (humanChoice) => {
+    const machineChoice = playMachine()
+    playTheGames(humanChoice, machineChoice)
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choices[randomNumber]
+}
+
+const playTheGames = (human, machine) => {
+    if (human === machine) {
+        result.innerHTML = 'Deu empate'
+    } else if (
+        (human === 'paper' && machine === 'rock') ||
+        (human === 'rock' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')
+    ) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = 'VOCÊ GANHOU!!!'
+
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = 'Você perdeu para a máquina'
+        
+    }
+}
